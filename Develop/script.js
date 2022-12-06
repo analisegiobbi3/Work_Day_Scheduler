a1b24b86019d0b71d96edc0421c87ab2eb71e7b7
Analise <analise.giobbi@gc.com>
2022-12-06 16:55:32 -0500

    attempt to save description text

--- Develop/script.js
+++ Develop/script.js
@@ -13,9 +13,9 @@ var present = $("present");
 var future = $("future");
 var hour = $("#hour");
 var today = dayjs();
-var todayTime = new Day();
-var todayHours = todayTime.getHours();
-var todayMinutes = odayTime.getMinutes();
+// var todayTime = new Day();
+// var todayHours = todayTime.getHours();
+// var todayMinutes = odayTime.getMinutes();
 
 $(function () {
   //Add code to display the current date in the header of the page.
@@ -27,31 +27,47 @@ $(function () {
   }, 1000);
 
   btnEl.click(function(){
-    //add function for saving and storing the text in events
+    var timeEl = $(".hour").val();
+    var textEl = $(".description").val();
+
+    localStorage.setItem(timeEl, textEl)
 
 
   })
 
   // will need to add a loop to look at each section and compare the hour text to the actual time
-  function getTime(){
-    if (hour === todayHours){
-      $(this).removeClass("past");
-      $(this).removeClass("future");
-      $(this).addClass("present");
+  // function getTime(){
+  //   if (hour === todayHours){
+  //     $(this).removeClass("past");
+  //     $(this).removeClass("future");
+  //     $(this).addClass("present");
   
-    }else if (hour < todayHours){
-      $(this).removeClass("present");
-      $(this).removeClass("future");
-      $(this).addClass("past");
+  //   }else if (hour < todayHours){
+  //     $(this).removeClass("present");
+  //     $(this).removeClass("future");
+  //     $(this).addClass("past");
   
-    }else if (hour > todayHours){
-      $(this).removeClass("past");
-      $(this).removeClass("present");
-      $(this).addClass("future");
-    }
-  }
+  //   }else if (hour > todayHours){
+  //     $(this).removeClass("past");
+  //     $(this).removeClass("present");
+  //     $(this).addClass("future");
+  //   }
+  // }
 
-  getTime();
+  // getTime();
+
+  //gets the value set in each 
+  $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  $("#hour-18 .description").val(localStorage.getItem("hour-18"));
 
 
 
